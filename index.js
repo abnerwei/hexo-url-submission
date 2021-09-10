961eb9f6a8e2e7dc312ffd047aee0d5899ec64d6
Abnerwei <abnerwei@vip.qq.com>
2021-09-10 17:34:53 +0800

    support shenma search engine

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 hexo.config.url_submission = Object.assign({
     enable: true,
     type: 'latest', // latest or all( latest: modified pages; all: posts & pages)
-    channel: ['baidu', 'bing', 'google'], // included channels are `baidu`, `google`, `bing`
+    channel: ['baidu', 'bing', 'google', 'shenma'], // included channels are `baidu`, `google`, `bing`, `shenma`
     prefix: ['post', 'wiki'],
     count: 10,
     proxy: '',
@@ -12,6 +12,8 @@ hexo.config.url_submission = Object.assign({
     baidu_token: '',
     bing_token: '',
     google_key: '',
+    shenma_token: '',
+    shenma_user: '',
     sitemap: '' // whereis it
 }, hexo.config.url_submission)
 
@@ -25,5 +27,6 @@ if (!config.enable) {
 hexo.extend.deployer.register('us_baidu_deployer', require('./lib/baidu_deployer')) // baidu api deployer
 hexo.extend.deployer.register('us_bing_deployer', require('./lib/bing_deployer')) // bing api deployer
 hexo.extend.deployer.register('us_google_deployer', require('./lib/google_deployer')) // google api deployer
+hexo.extend.deployer.register('us_shenma_deployer', require('./lib/shenma_deployer')) // google api deployer
 
 hexo.extend.generator.register('us_url_generator', require('./lib/generator')) // generator
